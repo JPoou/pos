3ed176ec5cbdd1e7737f0904147304305d944095
Juan Carlos Morales <jpoou@researchmobile.co>
2018-11-04 14:55:13 -0600

    Add model Orde and OrdeProduct class

---
+++ src/main/java/dev/poou/pos/model/Orde.java
@@ -0,0 +1,134 @@
+package dev.poou.pos.model;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EntityListeners;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.constraints.NotBlank;
+
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@Entity
+@Table(name="ordes")
+@EntityListeners(AuditingEntityListener.class)
+@JsonIgnoreProperties(value = { "createdAt", "updatedAt" }, allowGetters = true)
+public class Orde {
+	
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private Long id;
+	
+	@NotBlank
+	private String name;
+	
+	@NotBlank
+	private String description;
+	
+	@NotBlank
+	private String address;
+
+	@NotBlank
+	private int amount;
+	
+	@NotBlank
+	private int userId;
+	
+	@NotBlank
+	private Date deliveryAt;
+	
+	
+	@Column(nullable = false, updatable = false)
+	@Temporal(TemporalType.TIMESTAMP)
+	@CreatedDate
+	private Date createdAt;
+
+	@Column(nullable = false)
+	@Temporal(TemporalType.TIMESTAMP)
+	@LastModifiedDate
+	private Date updatedAt;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public int getAmount() {
+		return amount;
+	}
+
+	public void setAmount(int amount) {
+		this.amount = amount;
+	}
+
+	public int getUserId() {
+		return userId;
+	}
+
+	public void setUserId(int userId) {
+		this.userId = userId;
+	}
+
+	public Date getDeliveryAt() {
+		return deliveryAt;
+	}
+
+	public void setDeliveryAt(Date deliveryAt) {
+		this.deliveryAt = deliveryAt;
+	}
+
+	public Date getCreatedAt() {
+		return createdAt;
+	}
+
+	public void setCreatedAt(Date createdAt) {
+		this.createdAt = createdAt;
+	}
+
+	public Date getUpdatedAt() {
+		return updatedAt;
+	}
+
+	public void setUpdatedAt(Date updatedAt) {
+		this.updatedAt = updatedAt;
+	}
+	
+}
---
+++ src/main/java/dev/poou/pos/model/OrderProduct.java
@@ -0,0 +1,25 @@
+package dev.poou.pos.model;
+
+
+public class OrderProduct {
+	
+	private Long ordeId;
+
+	private Long productId;
+
+	public Long getOrdeId() {
+		return ordeId;
+	}
+
+	public void setOrdeId(Long ordeId) {
+		this.ordeId = ordeId;
+	}
+
+	public Long getProductId() {
+		return productId;
+	}
+
+	public void setProductId(Long productId) {
+		this.productId = productId;
+	}
+}
